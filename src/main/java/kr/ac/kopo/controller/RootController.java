4658552bc4a2c92d7963bfd11f646ffd77a8a0aa
jonghoon <md05172@naver.com>
2024-10-21 15:35:15 +0900

    make index.jsp And service.list()

--- src/main/java/kr/ac/kopo/controller/RootController.java
+++ src/main/java/kr/ac/kopo/controller/RootController.java
@@ -1,13 +1,22 @@
 package kr.ac.kopo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.ac.kopo.service.BookService;
 
 @Controller
 public class RootController {
 	
+	@Autowired
+	BookService service;
+	
 	@GetMapping("/")
-	String index() {
+	String index(Model model) {
+		model.addAttribute("list", service.list());
+		
 		return "index";
 	}
 }
