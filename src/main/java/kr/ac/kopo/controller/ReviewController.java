35962571e2f1e81a817a3ac57e97380a6b3c33f3
jonghoon <md05172@naver.com>
2024-11-08 13:18:52 +0900

    2024-11-08 저장

--- src/main/java/kr/ac/kopo/controller/ReviewController.java
+++ src/main/java/kr/ac/kopo/controller/ReviewController.java
@@ -1,19 +1,34 @@
 package kr.ac.kopo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.ac.kopo.model.Review;
+import kr.ac.kopo.model.ReviewService;
 
 @RestController
 @RequestMapping("/review")
 public class ReviewController {
 
+	@Autowired
+	ReviewService service;
+	
 	@PostMapping
 	public Review add(@RequestBody Review review) {
-		System.out.println("review " + review);
-		return review;
+		service.add(review);
+		
+		return service.item(review.getId());
+	}
+	
+	@GetMapping
+	public List<Review> list() {
+	
+		return service.list();
 	}
 }
---
+++ src/main/java/kr/ac/kopo/service/ReviewServiceImpl.java
@@ -0,0 +1,33 @@
+package kr.ac.kopo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.ac.kopo.dao.ReviewDao;
+import kr.ac.kopo.model.Review;
+import kr.ac.kopo.model.ReviewService;
+
+@Service
+public class ReviewServiceImpl implements ReviewService{
+
+	@Autowired
+	ReviewDao dao;
+	
+	@Override
+	public void add(Review review) {
+		dao.add(review);
+	}
+
+	@Override
+	public List<Review> list() {
+		return dao.list();
+	}
+
+	@Override
+	public Review item(Long id) {
+		return dao.item(id);
+	}
+
+}
