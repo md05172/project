573d001dec7de458cbeed6a95fa66cf40342cb5f
kimjonghoon <112178940+md05172@users.noreply.github.com>
2025-03-13 09:37:30 +0900

    Update KakaoService.java

--- src/main/java/kr/ac/kopo/service/KakaoService.java
+++ src/main/java/kr/ac/kopo/service/KakaoService.java
@@ -21,7 +21,7 @@ import kr.ac.kopo.model.Customer;
 @Service
 public class KakaoService {
 
-	public static final String KAKAOAPIKEY = "92bef2ab9e055a604cd9efcdafc5551e";
+	public static final String KAKAOAPIKEY = "나의키값";
 	public static final String REDIRECTURI = "http://172.16.145.10:9090/kakao/callback";
 	public static final String LOGOUT = "http://172.16.145.10:9090/kakao/logout";
 	public static final String KAKAOURL = "https://kauth.kakao.com/oauth/authorize?response_type=code&" + "client_id="
